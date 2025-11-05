8bf7e57c53217a0ef0ca57f066aef47e52f649f6
Andras Pahi <pahihu@gmail.com>
2025-11-05 19:46:12 +0100

    fixed mainframe_operations.sh path

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -17,7 +17,7 @@ fi
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.19.jar"
 
 # Upload mainframe script
-zowe zos-files upload file-to-uss "./github/scripts/mainframe_operations.sh" "/z/$LOWERCASE_USERNAME/mainframe_operations.sh"
+zowe zos-files upload file-to-uss "./.github/scripts/mainframe_operations.sh" "/z/$LOWERCASE_USERNAME/mainframe_operations.sh"
 
 # Verify upload
 echo "Verifying upload:"
