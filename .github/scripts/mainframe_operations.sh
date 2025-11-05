e1d1f244a068ef995843e520c2af8aa244f8546c
Andras Pahi <pahihu@gmail.com>
2025-11-05 20:39:48 +0100

    added JCL for EMPPAY and DEPTPAY

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -41,7 +41,7 @@ run_cobolcheck() {
     # Check if CC##99.CBL was created, regardless of cobolcheck exit status
     if [ -f "./testruns/CC##99.CBL" ]; then
         # Copy to the MVS dataset
-        if cp "CC##99.CBL" "//'${ZOWE_USERNAME}.CBL($program)'"; then
+        if cp "./testruns/CC##99.CBL" "//'${ZOWE_USERNAME}.CBL($program)'"; then
             echo "Copied CC##99.CBL to ${ZOWE_USERNAME}.CBL($program)"
         else
             echo "Failed to copy CC##99.CBL to ${ZOWE_USERNAME}.CBL($program)"
